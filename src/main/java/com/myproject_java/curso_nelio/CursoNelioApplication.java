2e4a2ef0b898c15d49995b84067f3a00fcd4ff5f
joao paulo <paulitu777@gmail.com>
2025-01-15 12:39:55 -0300

    project created

---
+++ src/main/java/com/myproject_java/curso_nelio/CursoNelioApplication.java
@@ -0,0 +1,13 @@
+package com.myproject_java.curso_nelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoNelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject_java/curso_nelio/CursoNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject_java.curso_nelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoNelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
